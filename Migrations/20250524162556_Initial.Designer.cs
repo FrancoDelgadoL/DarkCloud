776217c7b79b6d2b71f9bd4c22481f98131674d3
miguelitoelreal <miguegara20@gmail.com>
2025-05-24 11:27:16 -0500

    fix: migración inicial regenerada y dependencias alineadas para despliegue en Render

--- Migrations/20250524162556_Initial.Designer.cs
+++ Migrations/20250524162556_Initial.Designer.cs
@@ -12,7 +12,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DarkCloud.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250524161611_Initial")]
+    [Migration("20250524162556_Initial")]
     partial class Initial
     {
         /// <inheritdoc />
